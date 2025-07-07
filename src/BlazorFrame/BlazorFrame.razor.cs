2c33c98d0b72a4b8de3e99808f865c08a2a269af
Tim Maes <maestim@cronos.be>
2025-07-07 14:15:16 +0200

    move javascript

--- src/BlazorFrame/BlazorFrame.razor.cs
+++ src/BlazorFrame/BlazorFrame.razor.cs
@@ -68,7 +68,7 @@ public partial class BlazorFrame : IAsyncDisposable
         {
             module = await JSRuntime.InvokeAsync<IJSObjectReference>(
               "import",
-              "/_content/BlazorFrame/js/BlazorFrame.js");
+              "/_content/BlazorFrame/BlazorFrame.js");
             objRef = DotNetObjectReference.Create(this);
             
             await module.InvokeVoidAsync(
